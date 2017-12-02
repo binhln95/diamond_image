2077dfe7c61a35782cac0131bc977512da27866a
binhln95 <binhln95@gmail.com>
2017-12-02 17:23:51 +0700

    config

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@
 #include <opencv/highgui.h>
 #include <opencv2/core/core.hpp>
 #include <ostream>
+// add new
 #include <QTime>
 #include <QTimer>
 #include <QLineEdit>
@@ -37,6 +38,7 @@ void MainWindow::on_pushButton_clicked()
 
     QPixmap pix(filename);
     ui->image->setPixmap(pix);
+    ui->image->setScaledContents(true);
 }
 void delay( int millisecondsToWait )
 {
@@ -51,11 +53,13 @@ void MainWindow::on_pushButton_2_clicked()
     QString QnumberFrame = ui->numberFrame->text();
     int numberFrame = QnumberFrame.toInt();
     cout << typeid(numberFrame).name() << endl;
+//    if (typeid(numberFrame).name() == 0){
+//        QMessageBox::information(this, tr("File name"), filename);
+//    }
     QString QSeconds = ui->seconds->text() ;
     float Seconds = QSeconds.toFloat();
     float rate = 0.9;
     String image = filename.toUtf8().constData();
-    cout << Seconds/numberFrame << endl;
     for (int i = 0; i < numberFrame; i++)
     {
         rate = rate - 0.9/numberFrame;
